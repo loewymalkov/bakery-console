dff2d803d874ad58d5a58f8e60fe269e26e260bd
Loewy Malkovich <loewymalkovich@Loewys-MacBook-Pro.local>
2019-09-27 10:36:52 -0700

    add if branching to catch value errors

--- Program.cs
+++ Program.cs
@@ -2,9 +2,9 @@
 
 namespace Bakery
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
           Bread loaf = new Bread("loaf", 5);
           Pastery croissant = new Pastery("croissant", 4);
@@ -12,8 +12,18 @@ namespace Bakery
             Console.WriteLine("----------------------------------------------\nWelcome To Pierre's Bakery!\n----------------------------------------------\nWould you like to purchase bread or pastery? (bread/pastery)");
             string userInput = Console.ReadLine();
 
+            if (userInput.ToLower() != "bread" || userInput.ToLower() != "pastery")
+            {
+              Console.WriteLine("We could not understand your order, please re-enter either 'bread' or 'pastery'.");
+              userInput = Console.ReadLine();
+            }
 
             if (userInput.ToLower() == "bread")
+            {
+              Console.WriteLine("You have ordered a" + loaf.GetName() + "of bread.");
+              Console.WriteLine("It will cost $" + loaf.GetPrice());
+            }
+            else if  (userInput.ToLower() == "pastery")
             {
               Console.WriteLine("You have ordered a" + loaf.GetName() + "of bread.");
               Console.WriteLine("It will cost $" + loaf.GetPrice());
