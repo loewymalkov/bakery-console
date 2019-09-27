cdb8d50f3edecb7f40fae136a43e758230789c2f
Loewy Malkovich <loewymalkovich@Loewys-MacBook-Pro.local>
2019-09-27 10:48:42 -0700

    remove static void order to simplify run order

--- Program.cs
+++ Program.cs
@@ -1,33 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Bakery
 {
     public class Program
     {
-        static void Main()
+      static void Main()
+      {
+        Bread loaf = new Bread("loaf", 5);
+        Pastery croissant = new Pastery("croissant", 4);
+
+        Console.WriteLine("----------------------------------------------\nWelcome To Pierre's Bakery!\n----------------------------------------------\n");
+        
+        Console.WriteLine("Would you like to purchase bread or pastery, or view your basket? (bread/pastery/basket)");
+        string userInput = Console.ReadLine();
+
+        if (userInput.ToLower() != "bread" || userInput.ToLower() != "pastery")
         {
-          Bread loaf = new Bread("loaf", 5);
-          Pastery croissant = new Pastery("croissant", 4);
-
-            Console.WriteLine("----------------------------------------------\nWelcome To Pierre's Bakery!\n----------------------------------------------\nWould you like to purchase bread or pastery? (bread/pastery)");
-            string userInput = Console.ReadLine();
-
-            if (userInput.ToLower() != "bread" || userInput.ToLower() != "pastery")
-            {
-              Console.WriteLine("We could not understand your order, please re-enter either 'bread' or 'pastery'.");
-              userInput = Console.ReadLine();
-            }
-
-            if (userInput.ToLower() == "bread")
-            {
-              Console.WriteLine("You have ordered a" + loaf.GetName() + "of bread.");
-              Console.WriteLine("It will cost $" + loaf.GetPrice());
-            }
-            else if  (userInput.ToLower() == "pastery")
-            {
-              Console.WriteLine("You have ordered a" + loaf.GetName() + "of bread.");
-              Console.WriteLine("It will cost $" + loaf.GetPrice());
-            }
+          Console.WriteLine("We could not understand your order, please re-enter either 'bread' or 'pastery'.");
+          userInput = Console.ReadLine();
         }
+
+        if (userInput.ToLower() == "view" || userInput.ToLower() == "basket")
+        {
+          Console.WriteLine()
+        }
+        else if (userInput.ToLower() == "bread")
+        {
+          Console.WriteLine("You have ordered a" + loaf.GetName() + "of bread.");
+          Console.WriteLine("It will cost $" + loaf.GetPrice());
+        }
+        else if  (userInput.ToLower() == "pastery")
+        {
+          Console.WriteLine("You have ordered a" + loaf.GetName() + "of bread.");
+          Console.WriteLine("It will cost $" + loaf.GetPrice());
+        }
+        
+
+        Dictionary<string, int> basket = new Dictionary<string, int>(0); 
+        
+      }
     }
 }
