8eec085bf7e2846003d8fbd90c5f4a44403980cb
Loewy Malkovich <loewymalkovich@Loewys-MacBook-Pro.local>
2019-09-27 11:30:28 -0700

    add new static void Order

--- Bakery/Product.cs
+++ Bakery/Product.cs
@@ -1,3 +1,4 @@
+
 namespace Bakery
 {
   class Bread
--- Program.cs
+++ Program.cs
@@ -5,43 +5,51 @@ namespace Bakery
 {
     public class Program
     {
-      static void Main()
+
+      public static void Order()
       {
         Bread loaf = new Bread("loaf", 5);
         Pastery croissant = new Pastery("croissant", 4);
-        Dictionary<string, int> basket = new Dictionary<string, int>(); 
+        Dictionary<string, int> basket = new Dictionary<string, int>();
 
-        Console.WriteLine("----------------------------------------------\nWelcome To Pierre's Bakery!\n----------------------------------------------\n");
-        
-        Console.WriteLine("Would you like to purchase bread or pastery, or view your basket? (bread/pastery/basket)");
+        Console.WriteLine("Would you like to purchase bread or a pastery, or view your basket? (bread/pastery/basket)");
         string userInput = Console.ReadLine();
-
-        if (userInput.ToLower() != "bread" || userInput.ToLower() != "pastery")
-        {
-          Console.WriteLine("We could not understand your order, please re-enter either 'bread' or 'pastery'.");
-          userInput = Console.ReadLine();
-        }
 
         if (userInput.ToLower() == "view" || userInput.ToLower() == "basket")
         {
-          Console.WriteLine();
+          Console.WriteLine(basket);
+          Order();
         }
         else if (userInput.ToLower() == "bread")
         {
-          Console.WriteLine("You have ordered a" + loaf.GetName() + "of bread.");
+          Console.WriteLine("You have ordered a " + loaf.GetName() + " of bread.");
           Console.WriteLine("It will cost $" + loaf.GetPrice());
           basket.Add(loaf.GetName(), loaf.GetPrice());
+          Order();
         }
         else if  (userInput.ToLower() == "pastery")
         {
-          Console.WriteLine("You have ordered a" + loaf.GetName() + "of bread.");
-          Console.WriteLine("It will cost $" + loaf.GetPrice());
+          Console.WriteLine("You have ordered a " + croissant.GetName() + " of bread.");
+          Console.WriteLine("It will cost $" + croissant.GetPrice());
           basket.Add(croissant.GetName(), croissant.GetPrice());
+          Order();
         }
-        
+        else
+        {
+          Console.WriteLine("We could not understand your order, please re-enter either 'bread' or 'pastery'.");
+          Order();
+        }
+      }
 
+      public static void Main()
+      {
+         
+
+        Console.WriteLine("----------------------------------------------\nWelcome To Pierre's Bakery!\n----------------------------------------------\n");
         
-        
+        Program.Order();
+
+
       }
     }
 }
